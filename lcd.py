2637e888cfad8ac7860673e518bd0021be41ce3a
ThanishFuard1994 <73976287+ThanishFuard1994@users.noreply.github.com>
2020-12-11 19:58:51 +0530

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -1,30 +1,44 @@
+import urllib3
+import yaml
+import numpy as np
+import cv2
+import mouse_example7_best_one_v2
 import sys
 import os, time
-#import pyrebase
+import pyrebase
 import lcddriver
 
-str_on_frame = "Vacant: %d Occupied: %d" % (2, 3)
+##########################
 
 
+################################
 
+config = {'text_overlay': True} 
 
-display = lcddriver.lcd()
+#while(cap.isOpened()):
+while(True):  
+    str_on_frame = "Vacant: %d Occupied: %d" % (2, 3)
 
-# # Main body of code
-try:
-	while True:
-		# # Remember that your sentences can only be 16 characters long!
-		print("Writing to display")
-		display.lcd_display_string(str_on_frame, 1) # Write line of text to first line of display
-		display.lcd_display_string("Demo Pi Guy code", 2) # Write line of text to second line of display
-		time.sleep(2)                                     # Give time for the message to be read
-		display.lcd_display_string("I am a display!", 1)  # Refresh the first line of display with a different message
-		time.sleep(2)                                     # Give time for the message to be read
-		display.lcd_clear()                               # Clear the display of any data
-		time.sleep(2)                                     # Give time for the message to be read
+	if config['text_overlay']:
+		display = lcddriver.lcd()
 
-except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
-	print("Cleaning up!")
-	display.lcd_clear()
+		# # Main body of code
+		try:
+			while True:
+				# # Remember that your sentences can only be 16 characters long!
+				print("Writing to display")
+				display.lcd_display_string("Greetings Human!", 1) # Write line of text to first line of display
+				display.lcd_display_string("Demo Pi Guy code", 2) # Write line of text to second line of display
+				time.sleep(2)                                     # Give time for the message to be read
+				display.lcd_display_string("I am a display!", 1)  # Refresh the first line of display with a different message
+				time.sleep(2)                                     # Give time for the message to be read
+				display.lcd_clear()                               # Clear the display of any data
+				time.sleep(2)                                     # Give time for the message to be read
+
+		except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+			print("Cleaning up!")
+			display.lcd_clear()
+   
+
 
    
