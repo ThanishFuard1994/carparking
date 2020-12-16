d303b7dc50a8c96a6aecb8941652a71f52c276b9
ThanishFuard1994 <73976287+ThanishFuard1994@users.noreply.github.com>
2020-12-16 13:15:48 +0530

    Update my_project_v3.py

--- my_project_v3.py
+++ my_project_v3.py
@@ -170,28 +170,33 @@ while(True):
         cv2.rectangle(frame_out, (1, 5), (240, 15),(255,0,0), 85) # top left white rectangle on o/p
         cv2.rectangle(frame_out, (6, 10), (238, 13),(255,127,0), 83)
         cv2.rectangle(frame_out, (6, 10), (235, 10),(255,255,255), 80)
-        str_on_frame = "Vacant: %d Occupied: %d" % (spot, occupied)
+        #str_on_frame = "Vacant: %d Occupied: %d" % (spot, occupied)
 
 
     if config['lcd_display']:
-        display = lcddriver.lcd()
+        str_on_frame = "Vacant: %d" % (spot)
+	str_on_frame2 = "Occupied: %d" % (occupied)
+	# Load the driver and set it to "display"
+	# If you use something from the driver library use the "display." prefix first
+	display = lcddriver.lcd()
 
-         # Main body of code
-        try:
-            while True:
-                # # Remember that your sentences can only be 16 characters long!
-                print("Writing to display")
-                display.lcd_display_string("Greetings Human!", 1) # Write line of text to first line of display
-                display.lcd_display_string("Demo Pi Guy code", 2) # Write line of text to second line of display
-                time.sleep(2)                                     # Give time for the message to be read
-                display.lcd_display_string("I am a display!", 1)  # Refresh the first line of display with a different message
-                time.sleep(2)                                     # Give time for the message to be read
-                display.lcd_clear()                               # Clear the display of any data
-                time.sleep(2)                                     # Give time for the message to be read
+	# Main body of code
+	try:
+		while True:
+			# Remember that your sentences can only be 16 characters long!
+        		print("Writing to display")
+        		display.lcd_display_string(str_on_frame, 1) # Write line of text to first line of display
+        		display.lcd_display_string(str_on_frame2, 2) # Write line of text to second line of display
+        		time.sleep(2)                                     # Give time for the message to be read
+        		display.lcd_display_string(str_on_frame, 1) # Write line of text to first line of display  # Refresh the first line of display with a different message
+        		display.lcd_display_string(str_on_frame2, 2)
+        		time.sleep(2)                                     # Give time for the message to be read
+        		display.lcd_clear()
+        		time.sleep(2)                                     # Give time for the message to be read
 
-        except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
-            print("Cleaning up!")
-            display.lcd_clear()
+	except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press ctrl+c), exit the program and cleanup
+		print("Cleaning up!")
+    		display.lcd_clear()
         
         
         cv2.putText(frame_out, str_on_frame, (5,30), cv2.FONT_HERSHEY_SIMPLEX, #3RD IS BOTTOM-LEFT CORNER OF THE TEXT
